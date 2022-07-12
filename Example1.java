885c1426e935ce0a130a5b75933435616dcdf361
Arunktr123 <109127788+Arunktr123@users.noreply.github.com>
2022-07-12 10:21:17 +0530

    Add files via upload

---
+++ Example1.java
@@ -0,0 +1,23 @@
+package Examples;
+
+import java.util.Arrays;
+
+public class Example1{
+	
+	public static void main(String[] args){
+		int number = 10;
+		long number1 = 1234567l ;
+		byte number2 = 12;
+			System.out.println(number1);	
+		System.out.println(number2);
+		int numbers[] = new int [6];
+		 numbers[0] = 10;
+		 numbers[1] = 20;
+		 numbers[5] = 30;
+		 System.out.println(Arrays.toString(numbers));
+		 System.out.println(numbers[5]);
+		 char name[] = {'a','b','s',};
+		 System.out.println(numbers[1]);
+				
+}
+}
