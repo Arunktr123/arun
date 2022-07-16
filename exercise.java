868d89ac0cd2479e7f820497822db25ad0a0f84c
Arunktr123 <109127788+Arunktr123@users.noreply.github.com>
2022-07-16 18:17:17 +0530

    Add files via upload

--- exercise.java
+++ exercise.java
@@ -2,79 +2,79 @@ package exercise;
 
 import java.lang.reflect.Array;
 import java.util.Iterator;
-import java.util.Scanner;
-;
-public class exercise{
-	
-	public static void main(String[] args){//
-		
-		//ouestion1
+import java.util.Scanner;;
+
+public class exercise {
+
+	private static int row;
+
+	public static void main(String[] args) {
+
+		// ouestion1
 //		
 //		int a = 7;
 //				
 //		for (int i = 1; i <=10; i++) {
 //System.out.println(a*i);
 //System.out.println(a-i);
-       //question 2
+		// question 2
 //char a1;
 //for (a1 = 'm';a1<='z';a1++)
 //{
 //	System.out.println(a1);
-	
 
 //question23
-	
+
 //int x = 5;
 //int x1 = ++x - x++ + --x;
 //System.out.println(x1);
-	
-	
-	//question18
-		//1)
-	//int a = 30;
-			//int b = 6;
-			
-			//int x=69>>>2;
-		
-			//System.out.println(++a-b--);
-			//System.out.println(a%b++);
-			//System.out.println(a*=b+5);
-			//System.out.println(x);
-		//question 19
-			
-			//int a = 28;
-			
-			//System.out.println(a+= a++ + ++a + --a + a--);
-		
-		//question20
-		//int x = 10;
-		//System.out.println(x=x++ * 2+3 * --x);
-		
-		//question21
+
+		// question18
+		// 1)
+		// int a = 30;
+		// int b = 6;
+
+		// int x=69>>>2;
+
+		// System.out.println(++a-b--);
+		// System.out.println(a%b++);
+		// System.out.println(a*=b+5);
+		// System.out.println(x);
+		// question 19
+
+		// int a = 28;
+
+		// System.out.println(a+= a++ + ++a + --a + a--);
+
+		// question20
+		// int x = 10;
+		// System.out.println(x=x++ * 2+3 * --x);
+
+		// question21
 //		int y =10;
 //		int z = (++y* (y++ + 5));
 //		System.out.println(z);
-		
-		//question 22
-		//char c = 'z';
+
+		// question 22
+		// char c = 'z';
 //		int ascii = c;
 //		System.out.println("the ascii value of " + c + "is" + ascii);
-		
-		//question 25
-		
+
+		// question 25
+
 //		int dividend = 60; int divisor = 6;
 //		int quotient = dividend / divisor;
 //		int remainder = dividend % divisor;
 //		System.out.println("the divisor is = " + quotient);
 //		System.out.println("the quotient is = " + remainder);
-		
+
 //	question 26
 //		long num1 = 10l;
 //		long num2 = -10l;
 //		System.out.println("num1="+ num1);
 //		System.out.println("num2="+ num2);
 //		
-		//question 27
+		// question 27
 //		int x,y,t;
 //		Scanner sc = new Scanner(System.in);
 //		System.out.println("enter the value of x and y" );
@@ -86,8 +86,7 @@ public class exercise{
 //		x = y ; 
 //		y = t ;
 //		System.out.println("after swapping numer:  "+x+"  "+y);
-		
-		
+
 //		question 28
 //		Scanner reader = new Scanner(System.in);
 //		System.out.print("enter the number:");
@@ -100,20 +99,16 @@ public class exercise{
 //			
 //			System.out.println(num + " is odd");}
 
-		
-		//	question 29
-		
+		// question 29
+
 //		char al = 'i';
 //		if (al == 'a' || al == 'e' || al == 'i' || al == 'o' || al == 'u')
 //			System.out.println(al+"is vowel");
 //		else
 //			System.out.println(al  +   "  is consonant");
-		
-		
-		
-		//question no:30
-		
-		
+
+		// question no:30
+
 //		int a,b,c,largest,temp;
 //		
 //		Scanner sc = new Scanner(System.in);
@@ -130,9 +125,8 @@ public class exercise{
 //		largest = c>temp?c:temp;
 //		System.out.println("the largest value is " + largest);
 
-		
-		//question 31
-		
+		// question 31
+
 //		double firstRoot = 0;double secondRoot = 0;
 //		Scanner reader = new Scanner(System.in);
 //		System.out.println("enter the value a");
@@ -145,7 +139,7 @@ public class exercise{
 //		double determinant = (b*b - 4*a*c); 
 //		double sqrt = Math.sqrt(determinant);
 //		
-		
+
 //		
 //		if(determinant>0) {
 //			firstRoot = (+b +(sqrt) / 2*a);
@@ -154,11 +148,8 @@ public class exercise{
 //		else if 
 //			(determinant ==0) {
 //			System.out.println("root is :: " + (-b + sqrt/ 2*a));}
-		
-		
-		
-		
-		//question no : 32
+
+		// question no : 32
 //		 int num;
 //		Scanner sc = new Scanner(System.in);
 //		System.out.println("enter the  number:");
@@ -170,18 +161,8 @@ public class exercise{
 //		else 
 //			System.out.println("neither positive nor negative");
 
-		
-		
-		
-		
-		
-		
-		//question no :		33
-		
-		
-		
-		
-		
+		// question no : 33
+
 //		Scanner sc = new Scanner(System.in);
 //		char ch;
 //		System.out.println("enter the  number or letter:");
@@ -193,13 +174,9 @@ public class exercise{
 //		else {
 //			System.out.println(ch+ "is not alphabet");
 //		
-		
-		
-		
+
 		// question no 34
-		
-	
-		
+
 //		Scanner sc = new Scanner(System.in);
 //		System.out.println("enter the number");
 //		int num = sc.nextInt();
@@ -210,10 +187,8 @@ public class exercise{
 //            i++;}
 //		System.out.println("factorial of the number: "+ fact);
 //		
-		
-		
-		
-		//question no: 35
+
+		// question no: 35
 //int i;
 //		int [] arr = new int[] {0,1,5,7,4};
 //		System.out.println("elements of a given array present on even position ");
@@ -223,10 +198,7 @@ public class exercise{
 //	
 //		}
 
-		
-		
-		
-		//questio no : 36
+		// questio no : 36
 //		int i;
 //		int [] arr = new int[] {1,2,3,4,5};
 //		System.out.println("elements of a given array present on odd position ");
@@ -237,12 +209,8 @@ public class exercise{
 //		
 //		
 //		
-		
-		
-		
-		
-		
-		//question no:37
+
+		// question no:37
 //		int i;
 //		int [] arr = new int[] {1,2,3,4,5};
 //		System.out.println("elements of a given array present on odd position ");
@@ -250,11 +218,10 @@ public class exercise{
 //				)
 //		{	
 //	System.out.println(arr[i]);}
-		
-		//qestion no:38
-	
-		
-		//Java Program to print the largest element in an array
+
+		// qestion no:38
+
+		// Java Program to print the largest element in an array
 //		
 //		int [] arr = new int [] {67,89};
 //		 
@@ -266,10 +233,9 @@ public class exercise{
 //		max = arr[i];}
 //		{System.out.println("largest number present in given array:" + max);}
 //		
-		
-		
-		//question no: 38
-		//Java Program to print the smallest element in an array
+
+		// question no: 39
+		// Java Program to print the smallest element in an array
 //     int [] arr = new int [] {67,89,403,538};
 //		 
 //		int min = arr[0];
@@ -279,19 +245,14 @@ public class exercise{
 //    	if(arr[0]> min)
 //		min = arr[0];}
 //		{System.out.println("smalest number present in given array:" + min);}
-		
-		
-		
-		//question no: 39
+
+		// question no: 40
 //		
 //		int arr[] = new int[] {1,2,3,4,5,6,7,8,9};
 //		System.out.println("elements in array:"+ arr.length);
-		
-		
-		
-		
-		//question no: 40
-		
+
+		// question no: 41
+
 //		
 //		int arr1[] = new int[] {1,2,3};
 //		int arr2[] = new int [arr1.length];
@@ -309,110 +270,32 @@ public class exercise{
 //		System.out.println("elements of new array");
 //		for ( i = 0;i<arr2.length;i++) {
 //			System.out.println(arr2[i] + "");}
-			
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-				
-		
-		
+
+		// question no :42
+
+		//
+		int rowNum = 9;
+		  
+		for (int row = 1; row <= rowNum; row++){
 			
 			
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	}
+	    for(int space = rowNum; space > row;space--){
 				
-		
-		
-		
-		
-		
-		
-		
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-		
-		
-		
-			
+			System.out.print("  ");
 			}
+			
+			
+		int i = 1;
+		for(; i < row; i++) {
+			System.out.print(i +" ");
+		}
+		for(;i > 0;i--) {
+			System.out.print(i +" ");
+		}
+				System.out.println();}
 		
-		
-		
-		
-		
+			
 
+	}
 
+	}
