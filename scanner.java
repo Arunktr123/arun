c6ace33a885822562c66d5341955a50d4ff8945b
Arunktr123 <109127788+Arunktr123@users.noreply.github.com>
2022-07-19 15:48:10 +0530

    Add files via upload

---
+++ scanner.java
@@ -0,0 +1,49 @@
+package Examples;
+
+import java.util.Scanner;
+
+public class scanner {
+	public static void main(String[] args){
+//		Scanner num = new Scanner(System.in);
+//		  
+//		
+//		System.out.println("what is your nmae? ");
+//		
+//      String name = num.nextLine();
+//      System.out.println("hello  " + name);
+//      System.out.println();
+//      
+//      
+//      System.out.println("how old are you?");
+//      String age = num.nextLine();
+//        
+//      System.out.println("your fathr name" );
+//      String name1 = num.nextLine();
+//      
+//      System.out.println("your mother name");
+//      String name2 = num.nextLine();
+//      
+//      
+//      int a = 10 ,b = 10;
+//      
+//      
+//      System.out.println(a==b);
+//      
+//      
+//      
+//      
+//      
+		int a = 25;
+		double sqrt = Math.sqrt(a);
+		System.out.println(sqrt);
+			
+
+
+
+
+
+
+
+}
+
+}
