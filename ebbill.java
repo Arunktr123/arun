5ab1964ad3de01db0f7b22216eb360800a553aba
Arunktr123 <109127788+Arunktr123@users.noreply.github.com>
2022-07-27 13:39:22 +0530

    Add files via upload

---
+++ ebbill.java
@@ -0,0 +1,51 @@
+package Examples;
+import java.util.Scanner;
+public class ebbill {
+	public static void main (String args[]) {
+		Scanner scan=new Scanner(System.in);
+		int unit;
+		System.out.println("enter unit:");
+	unit = scan.nextInt();
+double billpay= 0;
+if(unit<100)
+	billpay = unit*1.50;
+else if (unit<200)
+	billpay = unit*3.00;
+else if (unit<300)
+	billpay = unit*5.00;
+System.out.println("BILL AMOUNT:"+billpay);
+
+
+
+
+
+
+
+		
+		
+	 
+		
+		
+		
+		
+		
+		
+		
+				
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+}
