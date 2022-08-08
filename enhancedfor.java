55654da5c4264db196f4d227766f8b5e50934bd9
Arunktr123 <109127788+Arunktr123@users.noreply.github.com>
2022-08-08 10:02:14 +0530

    Add files via upload

---
+++ enhancedfor.java
@@ -0,0 +1,17 @@
+package methods;
+
+import java.util.Scanner;
+
+public class enhancedfor {
+	public static void main (String args[]) {
+		
+		int num []= {10,11,12,13,14,15};
+		for(int a:num)
+		{
+			;System.out.println(a);
+		}
+		
+				
+	}
+
+}
