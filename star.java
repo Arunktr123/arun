cd4c5f4f48e0b85279106edcbaaa3038fa9d8d13
Arunktr123 <109127788+Arunktr123@users.noreply.github.com>
2022-08-09 17:11:35 +0530

    Add files via upload

---
+++ star.java
@@ -0,0 +1,17 @@
+package methods;
+
+public class star {
+public static void main(String args []) {
+	int i;
+	for (i=0;i<10;i++) {
+		for(int j=9;j<10;j++)
+		{
+		System.out.println
+		(" *");
+		}
+		System.out.println();
+
+	}
+	
+}
+}
